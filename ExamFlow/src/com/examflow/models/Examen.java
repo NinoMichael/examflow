422950ed8eac54f736693a358a8947478437c7df
Tantely <tandrianaridera@gmail.com>
2024-10-05 16:51:10 +0300

    create model examen

---
+++ ExamFlow/src/com/examflow/models/Examen.java
@@ -0,0 +1,58 @@
+package com.examflow.models;
+
+import java.time.LocalDateTime;
+
+public class Examen {
+	public int id;
+	public String code;
+	public String theme;
+	public LocalDateTime debut;
+	public LocalDateTime fin;
+	public String instruction;
+	public Enseignant enseignant;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getTheme() {
+		return theme;
+	}
+	public void setTheme(String theme) {
+		this.theme = theme;
+	}
+	public LocalDateTime getDebut() {
+		return debut;
+	}
+	public void setDebut(LocalDateTime debut) {
+		this.debut = debut;
+	}
+	public LocalDateTime getFin() {
+		return fin;
+	}
+	public void setFin(LocalDateTime fin) {
+		this.fin = fin;
+	}
+	public String getInstruction() {
+		return instruction;
+	}
+	public void setInstruction(String instruction) {
+		this.instruction = instruction;
+	}
+	public Enseignant getEnseignant() {
+		return enseignant;
+	}
+	public void setEnseignant(Enseignant enseignant) {
+		this.enseignant = enseignant;
+	}
+	
+	
+}
